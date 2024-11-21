6c784d19bb5903ab58911e02dc9f100e7b705487
ZebaAnwer <165253475+ZebaAnwer@users.noreply.github.com>
2024-11-20 21:02:06 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+ console.log(isEven(4)); // Output: true
+console.log(isEven(7)); // Output: false
+console.log(isEven(0)); // Output: true
+console.log(isEven(-5)); // Output: false
+ function isEven(number) {
+  return number % 2 === 0;
+}
